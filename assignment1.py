04d51256391882d3e7d7a55d9500a8ab8b29c074
divyakhandelwal2211 <khandelwaldivya2005@gmail.com>
2024-05-27 13:32:35 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,32 @@
+# How do you concatenate two strings in Python?
+
+s1 = 'this is'
+s2 = ' divya'
+c = s1 + s2 
+print(c) 
+
+# What method would you use to convert a string to lowercase in python?
+ 
+s = 'HELLO MY NAME IS DIVYA'
+print(s.lower())
+
+# How can you find the length of a string in Python?
+
+s = 'my name is divya'
+print(len(s))
+
+# Remove the white space from left side of given string
+st = " hello LIET "
+print(st.lstrip())
+
+# What is the difference between the "insert()" and "append()" methods when adding elements to a list in Python, give an example by coding.
+
+list = [1, 2, 3]
+list.append(4)
+print("After append:", list)
+list.insert(1, 'g')
+print("After insert:",list) 
+
+
+
+
